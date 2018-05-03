44742dc4524e1d2414057d9d855e9c15833a401e
benefit50 <d.godiac@webdelo.org>
2018-05-03 12:50:13 +0300

    Added RepositoryServiceProvider

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Providers;
+
+use App\Contracts\RepositoryContract;
+use App\Repositories\Github;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Cache\Repository;
+use Madewithlove\IlluminatePsrCacheBridge\Laravel\CacheItemPool;
+use Psr\Cache\CacheItemPoolInterface;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        /**
+         * Performing an instance-to-interface bind for the sake of switching repository source with ease.
+         * Now, anywhere we'll ask laravel to retrieve RepositoryContract from service container, we'll
+         * receive a class implementing that interface.
+         */
+        $this->app->bind(RepositoryContract::class, function () {
+            $cachePool = $this->app->make(CacheItemPoolInterface::class);
+
+            return new Github($cachePool);
+        });
+
+        $this->app->singleton(CacheItemPoolInterface::class, function ($app) {
+            $repository = $app->make(Repository::class);
+
+            return new CacheItemPool($repository);
+        });
+    }
+}
